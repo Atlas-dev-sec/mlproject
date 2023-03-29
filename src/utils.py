c33ee5d84901de246274682a6e4d9e4331f6d932
Atlas-dev-sec <atlas.software.developer@gmail.com>
2023-03-29 17:18:11 -0500

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -29,7 +29,7 @@ def evaluate_models(X_train, y_train,X_test, y_test, models, param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) # train model
-            
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
@@ -39,5 +39,13 @@ def evaluate_models(X_train, y_train,X_test, y_test, models, param):
 
         return report 
     
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
